02b4dc61c8ef2ba046836edd3bf16f0870bc0075
hardroks <scarelies@gmail.com>
2020-12-09 09:39:33 +0300

    WebDriverUpdated

--- WebDriverLab/WebDriverLab/Tests.cs
+++ WebDriverLab/WebDriverLab/Tests.cs
@@ -24,27 +24,48 @@ namespace WebDriverLab
             driver.Navigate().GoToUrl("https://xistore.by");
         }
 
-
         [Test]
         public void Test1()
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-            IWebElement PhonesLink = driver.FindElement(By.LinkText("Телефоны"));
-            PhonesLink.Click();
+            IWebElement Phones = driver.FindElement(By.LinkText("Телефоны"));
+            Phones.Click();
 
-            IWebElement RedmiNote8Link = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Redmi Note 8")));
-            RedmiNote8Link.Click();
+            IWebElement RedmiNote8 = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Redmi Note 8")));
+            RedmiNote8.Click();
 
-            IWebElement AccessoriesBlock = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("accesuary-item")));
-            AccessoriesBlock.Click();
+            IWebElement AddRedmiNote8ToCart = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("bx_2369550383_10342_buy_link")));
+            AddRedmiNote8ToCart.Click();
 
-            Assert.NotNull(driver.FindElement(By.Id("aces-block")));
+            IWebElement Earphones = wait.Until(ExpectedConditions.ElementToBeClickable(By.PartialLinkText("Аудио")));
+            Earphones.Click();
+
+            IWebElement RedmiAirDots = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Беспроводные наушники Redmi AirDots")));
+            RedmiAirDots.Click();
+
+            IWebElement AddRedmiAirDotsToCart = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("bx_2369550383_8393_buy_link")));
+            AddRedmiAirDotsToCart.Click();
+
+            IWebElement BasketItems = wait.Until(ExpectedConditions.ElementExists(By.Id("basket_items")));
+
+            string RedmiNote8Price = BasketItems.FindElement(By.XPath("tbody/tr[1]/td[3]/div[1]")).Text;
+            string RedmiAirDotsPrice = BasketItems.FindElement(By.XPath("tbody/tr[2]/td[3]/div[1]")).Text; 
+            string SummarizedPrice = driver.FindElement(By.Id("allSum_FORMATED")).Text;
+
+            Assert.AreEqual(GetPrice(SummarizedPrice), GetPrice(RedmiNote8Price)+GetPrice(RedmiAirDotsPrice));
         }
 
         [TearDown]
         public void TearDownTests()
         {
+            if (driver != null)
+                driver.Quit();
+        }
+        int GetPrice(string price)
+        {
+            price = price.Split(',')[0];
+            return int.Parse(new string(price.Where(t => char.IsDigit(t)).ToArray()));
         }
 
     }
